2abc85db33caf3103d404ed97e84ef11c2ef2a6c
Ribeh BLAYTI <ribeh.blayti@sofrecom.com>
2025-11-11 05:49:29 +0100

    feat:add devcontainers & remove cache

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -10,9 +10,11 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class TaskController extends AbstractController
 {
+    #[Route("/task/new", name="task_new")]
     public function new(Request $request): Response
     {
         // creates a task object and initializes some data for this example
